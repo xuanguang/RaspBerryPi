ca465a99f355df995b77d52b82b85fae57ecc190
LvYiguang <lvyiguang@163.com>
2017-09-27 22:58:19 +0800

    +

--- SenseHAT/officialDemo/printEnv.py
+++ SenseHAT/officialDemo/printEnv.py
@@ -3,8 +3,16 @@ from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
 
-temp = sense.get_temperature()
-print(temp)
+tempUnit = ' Celsius'
+temp = round(sense.get_temperature(), 1)
+#sense.show_message(str(int(temp))+"#")
+print(str(temp)+tempUnit)
 
-humidity = sense.get_humidity()
-print(humidity)
+humidityUnit = ' %'
+humidity = round(sense.get_humidity(),1)
+#sense.show_message(str(int(humidity))+"%")
+print(str(humidity)+humidityUnit)
+
+pressureUnit = ' millibars'
+pressure = round(sense.get_pressure(),1)
+print(str(pressure)+pressureUnit)
